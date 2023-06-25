8d1e06626300815c15131a1c2fd265b60fc0ec19
“damoncai” <782099197@qq.com>
2023-06-25 18:20:17 +0800

    初始化脚手架

---
+++ src/store/modules/user.ts
@@ -0,0 +1,26 @@
+//创建用户相关的小仓库
+import { defineStore } from 'pinia'
+
+import type { UserState } from './types/type'
+
+//创建用户小仓库
+const useUserStore = defineStore('User', {
+  //小仓库存储数据地方
+  state: (): UserState => {
+    return {
+      token: '', //用户唯一标识token
+      menuRoutes: [], //仓库存储生成菜单需要数组(路由)
+      username: '',
+      avatar: '',
+      //存储当前用户是否包含某一个按钮
+      buttons: [],
+    }
+  },
+  //异步|逻辑的地方
+  actions: {
+
+  },
+  getters: {},
+})
+//对外暴露获取小仓库方法
+export default useUserStore
---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Home
+  </div>
+</template>
+
+<script setup lang="ts">
+import useUserStore from '@/store/modules/user';
+//获取存储用户信息的仓库对象
+let userStore = useUserStore();
+</script>
+
+<style scoped>
+
+</style>
