676a8fc6352f1352334c7433b8fd14c22d74ec68
782099197qq.com <49423124+ximenxiaolanggou@users.noreply.github.com>
2023-08-14 08:53:19 +0800

    1

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const {app , BrowserWindow} = require('electron')
 
 const createWindow = () => {
-  const win = new BrowserWindow()({
+  const win = new BrowserWindow({
     width: 1000,
     height: 800
   })
+
   win.loadURL("http://localhost:5173/")
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
+}).catch(err => {
+  console.log(111)
+  console.log(err)
 })
