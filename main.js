65686acea4d8a1919fee7acc5a18b86a5a5de8e9
782099197qq.com <49423124+ximenxiaolanggou@users.noreply.github.com>
2024-01-29 10:47:53 +0800

    vditor

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const {app , BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1000,
-    height: 800
+    height: 800,
   })
 
-  win.loadURL("http://localhost:5173/")
+  win.loadURL('http://localhost:5173/')
 }
 
-app.whenReady().then(() => {
-  createWindow()
-}).catch(err => {
-  console.log(111)
-  console.log(err)
-})
+app
+  .whenReady()
+  .then(() => {
+    createWindow()
+  })
+  .catch((err) => {
+    console.log(111)
+    console.log(err)
+  })
